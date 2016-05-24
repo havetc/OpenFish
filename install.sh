6d1a044fd713c74f01a4408b0d83db18383daab9
Corentin Havet <Corentin.havet@ensimag.imag.fr>
2016-05-24 21:30:50 +0200

    add the possibility to update in install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
+wget https://github.com/havetc/OpenFish/archive/master.zip
+unzip master.zip
+mv -f OpenFish-master/* .
+rm -rf OpenFish-master/
+rm master.zip
 sudo apt-get -y install libqt4-dev
 sudo apt-get -y install libopencv-dev
 sudo apt-get -y install libxvidcore4
