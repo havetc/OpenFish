0ee7e9439b6ca42dc9e0d401a349583401532a4e
corentin havet <corentin.havet@hotmail.fr>
2017-02-25 01:46:25 +0100

    adding the russian translation

--- lang/OpenFish_ru.ts
+++ lang/OpenFish_ru.ts
@@ -6,37 +6,37 @@
     <message>
         <location filename="../getsize.ui" line="32"/>
         <source>Résolution de la vidéo convertie</source>
-        <translation type="unfinished"></translation>
+        <translation>Разрешение конвертированного видео</translation>
     </message>
     <message>
         <location filename="../getsize.ui" line="116"/>
         <source>Largeur</source>
-        <translation type="unfinished"></translation>
+        <translation>Ширина</translation>
     </message>
     <message>
         <location filename="../getsize.ui" line="129"/>
         <source>hauteur</source>
-        <translation type="unfinished"></translation>
+        <translation>Высота</translation>
     </message>
     <message>
         <location filename="../getsize.ui" line="142"/>
         <source>Résolution d&apos;entrée</source>
-        <translation type="unfinished"></translation>
+        <translation>Исходное разрешение</translation>
     </message>
     <message>
         <location filename="../getsize.ui" line="161"/>
         <source>Carré selon largeur de l&apos;entrée</source>
-        <translation type="unfinished"></translation>
+        <translation>Соотношение 1:1 по исходной ширине</translation>
     </message>
     <message>
         <location filename="../getsize.ui" line="177"/>
         <source>Carré selon hauteur de l&apos;entrée</source>
-        <translation type="unfinished"></translation>
+        <translation>Соотношение 1:1 по исходной высоте</translation>
     </message>
     <message>
         <location filename="../getsize.ui" line="193"/>
         <source>Autre :</source>
-        <translation type="unfinished"></translation>
+        <translation>Другое:</translation>
     </message>
 </context>
 <context>
@@ -44,172 +44,172 @@
     <message>
         <location filename="../mainwindow.ui" line="20"/>
         <source>OpenFish</source>
-        <translation type="unfinished"></translation>
+        <translation>OpenFish</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="60"/>
         <source>hauteur °</source>
-        <translation type="unfinished"></translation>
+        <translation>Наклон °</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="106"/>
         <source>zoom %</source>
-        <translation type="unfinished"></translation>
+        <translation>Масштаб(zoom) %</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="158"/>
         <source>correction fov %</source>
-        <translation type="unfinished"></translation>
+        <translation>исправление fov %</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="230"/>
         <source>Nombre de frame</source>
-        <translation type="unfinished"></translation>
+        <translation>Количество кадров</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="291"/>
         <source>Orientation</source>
-        <translation type="unfinished"></translation>
+        <translation>Ориентация</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="342"/>
         <source>Conversion</source>
-        <translation type="unfinished"></translation>
+        <translation>Конвертировать</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="384"/>
         <source>%p%</source>
-        <translation type="unfinished"></translation>
+        <translation>%p%</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="397"/>
         <source>Apercu:</source>
-        <translation type="unfinished"></translation>
+        <translation>Предварительный просмотр:</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="472"/>
         <source>&amp;Fichier</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Файл</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="479"/>
         <source>&amp;Options</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Параметры</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="489"/>
         <source>Langage</source>
-        <translation type="unfinished"></translation>
+        <translation>Язык</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="502"/>
         <source>toolBar</source>
-        <translation type="unfinished"></translation>
+        <translation>toolBar</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="527"/>
         <source>&amp;Ouvrir fichier</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Открыть файл</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="535"/>
         <source>&amp;Fermer</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Закрыть</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="540"/>
         <source>&amp;Résolution</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Разрешение</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="551"/>
         <source>Avec audio</source>
         <comment>with sound</comment>
-        <translation type="unfinished"></translation>
+        <translation>Со звуком</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="556"/>
         <source>Français</source>
-        <translation type="unfinished"></translation>
+        <translation>Французский</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="561"/>
         <source>Anglais</source>
-        <translation type="unfinished"></translation>
+        <translation>Английский</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="566"/>
         <source>Russe</source>
-        <translation type="unfinished"></translation>
+        <translation>Русский</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="101"/>
         <location filename="../mainwindow.cpp" line="103"/>
         <source>Sélectionner la vidéo à traiter</source>
-        <translation type="unfinished"></translation>
+        <translation>Выбрать видео для обработки</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="108"/>
         <location filename="../mainwindow.cpp" line="134"/>
         <source>Attention</source>
-        <translation type="unfinished"></translation>
+        <translation>Внимание</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="108"/>
         <location filename="../mainwindow.cpp" line="134"/>
         <source>Le fichier: </source>
-        <translation type="unfinished"></translation>
+        <translation>Файл: </translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="108"/>
         <source> n&apos;exite pas</source>
-        <translation type="unfinished"></translation>
+        <translation>не существует</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="114"/>
         <source>Vidéo Ouverte</source>
-        <translation type="unfinished"></translation>
+        <translation>Видео открыто</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="134"/>
         <source> n&apos;est pas un fichier vidéo valide</source>
-        <translation type="unfinished"></translation>
+        <translation>недействительный</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="164"/>
         <source>error</source>
-        <translation type="unfinished"></translation>
+        <translation>ошибка</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="164"/>
         <source>output resolution non determined</source>
-        <translation type="unfinished"></translation>
+        <translation>Информация</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="213"/>
         <location filename="../mainwindow.cpp" line="215"/>
         <source>Info</source>
-        <translation type="unfinished"></translation>
+        <translation>Информация</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="213"/>
         <source>Conversion terminée</source>
-        <translation type="unfinished"></translation>
+        <translation>Конвертирование завершено</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="215"/>
         <source>Conversion terminée, sans son</source>
-        <translation type="unfinished"></translation>
+        <translation>Конвертирование завершено, без звука</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="265"/>
         <source>Problème son</source>
-        <translation type="unfinished"></translation>
+        <translation>Проблема со звуком</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="265"/>
         <source>Erreur de ffmpeg, le fichier est convertit mais sans son</source>
-        <translation type="unfinished"></translation>
+        <translation>Ошибка ffmpeg, файл  конвертирован, но без звука</translation>
     </message>
 </context>
 </TS>
